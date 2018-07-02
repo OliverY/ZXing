d5dc3bf101d6f8efde0ff197929eec0b1f169b6c
yanxujun <yanxujun@yuantutech.com>
2018-07-02 22:50:15 +0800

    增加dialog

--- app/src/main/java/com/yuantu/zxing/BaseActivity.java
+++ app/src/main/java/com/yuantu/zxing/BaseActivity.java
@@ -3,8 +3,6 @@ package com.yuantu.zxing;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 
-import com.yuantu.zxing.widget.ProgressUtils;
-
 /**
  * Author:  Yxj
  * Time:    2018/7/2 下午10:24
